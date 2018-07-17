0906c4d1c887a1806c776f8972901f8de727e95e
Daniele Carcasole <daniele@pop-os.localdomain>
2018-07-17 11:21:41 +0200

    Debian 9 Stretch Base added

---
+++ debian9-stretch/scripts/base.sh
@@ -0,0 +1,39 @@
+# Adjust root filesystem
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Update the box
+apt-get -qqy update
+apt-get -qqy upgrade
+apt-get -qqy install linux-headers-$(uname -r) build-essential
+
+# Install development tools
+apt-get install -qqy unzip curl jq net-tools dnsutils curl wget vim \
+  mc screen lsof htop iotop dstat telnet tcpdump jq gnupg git 
+  
+
+# Remove unneeded items
+apt-get -qy purge exim4 exim4-base
+
+# Set up sudo
+echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
+
+# Disable barriers on root filesystem
+sed -i 's/noatime,errors/nobarrier,noatime,nodiratime,errors/' /etc/fstab
+
+# Tweak sshd to prevent DNS resolution (speed up logins)
+echo 'UseDNS no' >> /etc/ssh/sshd_config
+
+# Remove 5s grub timeout to speed up booting
+cat <<EOF > /etc/default/grub
+# If you change this file, run 'update-grub' afterwards to update
+# /boot/grub/grub.cfg.
+
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX="debian-installer=en_US"
+EOF
+
+update-grub
